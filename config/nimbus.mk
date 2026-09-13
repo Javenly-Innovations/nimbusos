c3cd79f2fb12fae81565c1b47148f2ec8bbdb499
Your Name <business@javenly.com>
2026-09-13 09:27:03 -0400

    Inital Commit

---
+++ config/nimbus.mk
@@ -0,0 +1,28 @@
+PRODUCT_PACKAGES += \
+    FossifyFileManager \
+    FossifyCamera \
+    FossifyCalculator \
+    FossifyVoiceRecorder \
+    FossifyDocuments \
+    FossifyClock \
+    FossifyGallery \
+    FossifyMessages \
+    FossifyPhone \
+    FossifyNotes \
+    FossifyCalendar \
+    FossifyContacts \
+    Firefox \
+    Aptoide
+
+PRODUCT_PACKAGES -= \
+    Calculator2 \
+    DeskClock \
+    Gallery3D \
+    Messaging \
+    Dialer \
+    Camera2 \
+    Recorder
+
+PRODUCT_PACKAGE_OVERLAYS := \
+    vendor/nimbus/overlay/common \
+    $(PRODUCT_PACKAGE_OVERLAYS)
